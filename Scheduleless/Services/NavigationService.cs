3e893fb19f23682037cbade8db3f49f463e2275a
Colin Petruno <colinpetruno@gmail.com>
2017-05-03 13:36:47 -0400

    Sign in if Authenticated
    This adds a way to auto login if the user has a valid token. The only
    downside is that this will only work for the first 2 hours before having
    to login again after closing the app. We may want to try a refresh
    before directing them to the login at a later point.
    
    Also fix a bug where the expires_in was not being returned. This
    resulted in all requests getting a new access token.

--- Scheduleless/Services/NavigationService.cs
+++ Scheduleless/Services/NavigationService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using Scheduleless.Renderers;
 using Scheduleless.Views;
@@ -9,6 +10,13 @@ namespace Scheduleless.Services
 	public class NavigationService
 	{
 		private static readonly Lazy<NavigationService> lazy = new Lazy<NavigationService>(() => new NavigationService());
+		private CredentialsService _credentialsService;
+
+		private NavigationService()
+		{
+			_credentialsService = new CredentialsService();
+		}
+
 		public static NavigationService Instance
 		{
 			get
@@ -21,11 +29,24 @@ namespace Scheduleless.Services
 
 		public Page GetInitialScreen()
 		{
-			// TODO: if user is already logged in - take them to the shifts page
-			var page = new LoginPage();
-			var navPage = new ThemedNavigationPage(page);
-			Navigation = navPage.Navigation;
-			return navPage;
+			Debug.WriteLine($"GetInitialScreen");
+
+			if (_credentialsService.IsAuthenticated)
+			{
+				Debug.WriteLine($"CredentialsSerivce Is Authenticated");
+				var page = new ShiftsPage();
+				var navPage = new ThemedNavigationPage(page);
+				Navigation = navPage.Navigation;
+				return navPage;
+			}
+			else
+			{
+				Debug.WriteLine($"CredentialsSerivce Is Not Authenticated");
+				var page = new LoginPage();
+				var navPage = new ThemedNavigationPage(page);
+				Navigation = navPage.Navigation;
+				return navPage;	
+			}
 		}
 
 		public async Task DisplayShiftsPageAsync()
