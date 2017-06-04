b41aded9767fbd7a7eb11865ea217583abdc22bd
Caliber <simon@lazilyinspired.com>
2017-06-04 18:15:42 -0400

    Push notifications iOS and android

---
+++ Droid/Providers/PushNotificationService.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Android.Gms.Common;
+using Firebase.Iid;
+using Scheduleless.Interfaces;
+
+[assembly: Xamarin.Forms.Dependency(typeof(Scheduleless.Droid.Providers.PushNotificationService))]
+namespace Scheduleless.Droid.Providers
+{
+	public class PushNotificationService : IPushNotificationService
+	{
+		public PushNotificationService() { }
+
+		public bool IsPlayServicesAvailable()
+		{
+			int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(MainActivity.CurrentActivity);
+			if (resultCode != ConnectionResult.Success)
+			{
+				if (GoogleApiAvailability.Instance.IsUserResolvableError(resultCode))
+					System.Diagnostics.Debug.WriteLine(GoogleApiAvailability.Instance.GetErrorString(resultCode));
+				else
+				{
+					System.Diagnostics.Debug.WriteLine("This device is not supported");
+				}
+				return false;
+			}
+			else
+			{
+				System.Diagnostics.Debug.WriteLine("Google Play Services is available.");
+				return true;
+			}
+		}
+
+		#region - IPushNotificationService
+
+		public string Token
+		{
+			get
+			{
+				return FirebaseInstanceId.Instance.Token;
+			}
+		}
+
+		public void Connect()
+		{
+			// no-op
+		}
+
+		public void Disconnect()
+		{
+			// no-op
+		}
+
+		public void HandleRegister(Action<bool, string> requestAuthPermissionCallback)
+		{
+			if (requestAuthPermissionCallback != null)
+			{
+				requestAuthPermissionCallback(IsPlayServicesAvailable(), Token);
+			}
+		}
+
+		public void OnTokenRefresh(object deviceToken, Action<string> completionHandler)
+		{
+			if (completionHandler != null && deviceToken != null)
+			{
+				completionHandler(deviceToken.ToString());
+			}
+		}
+
+		#endregion
+	}
+}
---
+++ Scheduleless/Services/PushNotificationService.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Scheduleless.Interfaces;
+using Xamarin.Forms;
+
+namespace Scheduleless.Services
+{
+	public class PushNotificationService
+	{
+		private static readonly Lazy<PushNotificationService> lazy = new Lazy<PushNotificationService>(() => new PushNotificationService());
+		public static PushNotificationService Instance
+		{
+			get
+			{
+				return lazy.Value;
+			}
+		}
+
+		private PushNotificationService() { }
+
+		public async Task UpdateUserPushTokenAsync(string deviceToken)
+		{
+			// TODO: Make call to server
+		}
+
+		public void OnTokenRefresh(object deviceToken)
+		{
+			Debug.WriteLine("OnTokenRefresh");
+			// TODO: Make call to server
+
+			//if (FirebaseService.Instance.CurrentUser == null)
+			//{
+			//	return;
+			//}
+
+			DependencyService.Get<IPushNotificationService>().OnTokenRefresh(
+				deviceToken,
+				async (registeredToken) =>
+				{
+					// TODO: Make call to server
+					//await FirebaseService.Instance.UpdateUserPushTokenAsync(registeredToken);
+				});
+		}
+
+		#region - IPushNotificationService
+
+		public string Token
+		{
+			get
+			{
+				return DependencyService.Get<IPushNotificationService>().Token;
+			}
+		}
+
+		public void HandleRegister(Action<bool, string> requestAuthPermissionCallback)
+		{
+			DependencyService.Get<IPushNotificationService>().HandleRegister(requestAuthPermissionCallback);
+		}
+
+		public void Connect()
+		{
+			DependencyService.Get<IPushNotificationService>().Connect();
+		}
+
+		public void Disconnect()
+		{
+			DependencyService.Get<IPushNotificationService>().Disconnect();
+		}
+
+		#endregion
+	}
+}
