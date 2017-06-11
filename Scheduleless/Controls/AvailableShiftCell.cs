c736d47a3940909e5ab098b16e1bb4f65e380db7
Colin Petruno <colinpetruno@gmail.com>
2017-06-11 00:58:53 -0400

    Style Available Shifts Index:

--- Scheduleless/Controls/AvailableShiftCell.cs
+++ Scheduleless/Controls/AvailableShiftCell.cs
@@ -9,6 +9,8 @@ namespace Scheduleless.Controls
         Label NameLabel;
         Label MonthLabel;
         Label DayLabel;
+        Label FromLabel;
+        Label NoteLabel;
         StackLayout MainLayout;
 
         public AvailableShiftCell()
@@ -25,35 +27,66 @@ namespace Scheduleless.Controls
                 FontAttributes = FontAttributes.None,
                 LineBreakMode = LineBreakMode.TailTruncation,
                 VerticalOptions = LayoutOptions.CenterAndExpand,
-                FontSize = 16,
-                TextColor = Color.Black
+                FontSize = 17,
+                TextColor = Color.FromHex("41454b")
+            };
+
+            FromLabel = new Label
+            {
+                BackgroundColor = Color.Transparent,
+                FontAttributes = FontAttributes.None,
+                LineBreakMode = LineBreakMode.TailTruncation,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                FontSize = 15,
+                TextColor = Color.FromHex("848d98")
+            };
+
+
+            NoteLabel = new Label
+            {
+                BackgroundColor = Color.Transparent,
+                FontAttributes = FontAttributes.None,
+                LineBreakMode = LineBreakMode.TailTruncation,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                FontSize = 15,
+                TextColor = Color.FromHex("848d98")
             };
 
             MonthLabel = new Label
             {
-                Text = "Jul",
-                TextColor = Color.Black,
+                TextColor = Color.FromHex("5f97ff"),
                 HorizontalOptions = LayoutOptions.Center,
-                VerticalOptions = LayoutOptions.CenterAndExpand,
+                VerticalOptions = LayoutOptions.Center,
                 FontSize = 18
             };
 
             DayLabel = new Label
             {
-                Text = "10",
-                TextColor = Color.Black,
+                TextColor = Color.FromHex("5f97ff"),
                 HorizontalOptions = LayoutOptions.Center,
-                VerticalOptions = LayoutOptions.CenterAndExpand,
-                FontSize = 18
+                VerticalOptions = LayoutOptions.Center,
+                FontSize = 30
+            };
+
+            var labelLayout = new StackLayout
+            {
+                Children = { NameLabel, FromLabel, NoteLabel },
+                BackgroundColor = Color.White,
+                Orientation = StackOrientation.Vertical,
+                HorizontalOptions = LayoutOptions.StartAndExpand,
+                VerticalOptions = LayoutOptions.CenterAndExpand
             };
 
             var frameLayout = new StackLayout
             {
                 Children = { MonthLabel, DayLabel },
+                BackgroundColor = Color.White,
                 Orientation = StackOrientation.Vertical,
                 HorizontalOptions = LayoutOptions.Center,
-                VerticalOptions = LayoutOptions.FillAndExpand,
-                Padding = 0
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                WidthRequest = 80,
+                HeightRequest = 76,
+                Padding = new Thickness(0, 12, 0, 0),
             };
 
             var frame = new Frame
@@ -61,26 +94,39 @@ namespace Scheduleless.Controls
                 Content = frameLayout,
                 OutlineColor = Color.Silver,
                 HasShadow = false,
+                BackgroundColor = Color.FromHex("5f97ff"),
                 CornerRadius = 0,
-                VerticalOptions = LayoutOptions.CenterAndExpand,
-                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                Padding = -5,
                 HeightRequest = 60,
-                WidthRequest = 60
+                WidthRequest = 102
             };
-
-
 
             MainLayout = new StackLayout
             {
-                Children = { frame, NameLabel },
+                Children = { frame, labelLayout },
                 Orientation = StackOrientation.Horizontal,
                 HeightRequest = 120,
-                HorizontalOptions = LayoutOptions.StartAndExpand,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
                 VerticalOptions = LayoutOptions.FillAndExpand,
-                Padding = new Thickness(10, 10, 10, 10)
+                Padding = new Thickness(0, 0, 0, 0),
+                Margin = new Thickness(10, 10, 10, 10),
+                BackgroundColor = Color.White
             };
 
-            View = MainLayout;
+            var outerFrame = new Frame
+            {
+                Content = MainLayout,
+                OutlineColor = Color.Silver,
+                HasShadow = false,
+                BackgroundColor = Color.White,
+                CornerRadius = 0,
+                Padding = 0,
+                Margin = new Thickness(15, 10, 15, 10),
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+            };
+
+            View = outerFrame;
         }
 
         private void SetupBindings()
@@ -89,6 +135,8 @@ namespace Scheduleless.Controls
             NameLabel.SetBinding<AvailableShift>(Label.TextProperty, availableShift => availableShift.ShiftLabel);
             MonthLabel.SetBinding<AvailableShift>(Label.TextProperty, availableShift => availableShift.ShiftShortMonth);
             DayLabel.SetBinding<AvailableShift>(Label.TextProperty, availableShift => availableShift.ShiftDate);
+            FromLabel.SetBinding<AvailableShift>(Label.TextProperty, availableShift => availableShift.OfferedByName);
+            NoteLabel.SetBinding<AvailableShift>(Label.TextProperty, availableShift => availableShift.Note);
         }
     }
 }
