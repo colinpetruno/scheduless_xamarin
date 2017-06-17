2a374e6a3fc9499f03ea3886de5b3882683edb09
Caliber <simon@lazilyinspired.com>
2017-06-17 14:15:52 -0400

    Rename class extensions with Extensions suffix

---
+++ Scheduleless/Extensions/JTokenExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace Scheduleless.Extensions
+{
+	public static class JTokenExtensions
+	{
+		// Reference: http://stackoverflow.com/questions/24066400/checking-for-empty-null-jtoken-in-a-jobject
+		public static bool IsNullOrEmpty(this JToken token)
+		{
+			return (token == null) ||
+				   (token.Type == JTokenType.Array && !token.HasValues) ||
+				   (token.Type == JTokenType.Object && !token.HasValues) ||
+				   (token.Type == JTokenType.String && token.ToString() == string.Empty) ||
+				   (token.Type == JTokenType.Null);
+		}
+	}
+}
