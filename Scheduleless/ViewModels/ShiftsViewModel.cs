cba8b979981a046af4fa5bbb8a02897c06259c1f
Caliber <simon@lazilyinspired.com>
2017-06-17 15:41:12 -0400

    Make calls parallel on Shifts Page

--- Scheduleless/ViewModels/ShiftsViewModel.cs
+++ Scheduleless/ViewModels/ShiftsViewModel.cs
@@ -10,224 +10,217 @@ using Xamarin.Forms;
 
 namespace Scheduleless.ViewModels
 {
-    public class ShiftsViewModel : BaseViewModel
-    {
-        Command _refreshCommand;
-        public Command RefreshCommand
-        {
-            get { return _refreshCommand; }
-        }
+	public class ShiftsViewModel : BaseViewModel
+	{
+		Command _refreshCommand;
+		public Command RefreshCommand
+		{
+			get { return _refreshCommand; }
+		}
 
-        private Boolean _dataLoaded = false;
-        public Boolean DataLoaded
-        {
-            get { return _dataLoaded; }
-            set { SetProperty(ref _dataLoaded, value); }
-        }
+		private Boolean _dataLoaded = false;
+		public Boolean DataLoaded
+		{
+			get { return _dataLoaded; }
+			set { SetProperty(ref _dataLoaded, value); }
+		}
 
-        // TODO: create a ShiftsService to manage all the shifts, but for now this is just POC
-        private List<FutureShift> _futureShifts = new List<FutureShift>();
-        public List<FutureShift> FutureShifts
-        {
-            get { return _futureShifts; }
-            set { SetProperty(ref _futureShifts, value); }
-        }
+		// TODO: create a ShiftsService to manage all the shifts, but for now this is just POC
+		private List<FutureShift> _futureShifts = new List<FutureShift>();
+		public List<FutureShift> FutureShifts
+		{
+			get { return _futureShifts; }
+			set { SetProperty(ref _futureShifts, value); }
+		}
 
-        private FutureShift _featuredShift;
-        public FutureShift FeaturedShift
-        {
-            get { return _featuredShift; }
-            set { SetProperty(ref _featuredShift, value); }
-        }
-
-
-
-
-        public string FeaturedShiftMonth
-        {
-            get
-            {
-                if (FeaturedShift != null)
-                {
-                    return FeaturedShift.Month;
-                }
-                else
-                {
-                    return "";
-                }
-
-            }
-        }
-
-        public string FeaturedShiftHours
-        {
-            get
-            {
-
-                return FeaturedShift.Label;
-            }
-        }
-
-        private FutureShiftsEndpoint _futureShiftsEndpoint;
-        private FeaturedShiftEndpoint _featuredShiftEndpoint;
-
-        public ShiftsViewModel()
-        {
-            _futureShiftsEndpoint = new FutureShiftsEndpoint();
-            _featuredShiftEndpoint = new FeaturedShiftEndpoint();
-            _refreshCommand = new Command(RefreshList);
-        }
-
-        async void RefreshList()
-        {
-            IsRefreshing = true;
-            IsBusy = true;
-            FutureShifts = await ExecuteFetchShiftsLiteAsync();
-            FeaturedShift = await ExecuteFetchFeaturedShiftLiteAsync();
-            IsBusy = false;
-            IsRefreshing = false;
-        }
-
-        Command _fetchAllDataCommand;
-        public Command FetchAllDataCommand
-        {
-            get { return _fetchAllDataCommand ?? (_fetchAllDataCommand = new Command(async () => await ExecuteFetchAllDataCommandAsync())); }
-        }
-
-        private async Task ExecuteFetchAllDataCommandAsync()
-        {
-            // TODO TODO TODO *IMPORTANT* MAKE THIS COMMAND WORK IN PARALLEL!!
-            if (IsBusy)
-            {
-                return;
-            }
-            DialogService.ShowLoading(string.Empty);
-
-            var response = await _futureShiftsEndpoint.IndexAsync<FutureShift>();
-            if (response.IsSuccess)
-            {
-                FutureShifts = response.Result.ToList();
-            }
-
-            var featured_response = await _featuredShiftEndpoint.FeaturedAsync<FutureShift>();
-            DialogService.HideLoading();
-
-            if (featured_response.IsSuccess)
-            {
-                FeaturedShift = featured_response.Result;
-            }
-            DataLoaded = true;
-            IsBusy = false;
-        }
-
-
-        async Task<List<FutureShift>> ExecuteFetchShiftsLiteAsync()
-        {
-            var response = await _futureShiftsEndpoint.IndexAsync<FutureShift>();
-
-            return response.Result.ToList();
-        }
-
-        async Task<FutureShift> ExecuteFetchFeaturedShiftLiteAsync()
-        {
-            var response = await _featuredShiftEndpoint.FeaturedAsync<FutureShift>();
-
-            return response.Result;
-        }
+		private FutureShift _featuredShift;
+		public FutureShift FeaturedShift
+		{
+			get { return _featuredShift; }
+			set { SetProperty(ref _featuredShift, value); }
+		}
 
 
 
 
+		public string FeaturedShiftMonth
+		{
+			get
+			{
+				if (FeaturedShift != null)
+				{
+					return FeaturedShift.Month;
+				}
+				else
+				{
+					return "";
+				}
 
+			}
+		}
 
-        Command _checkInCommand;
-        public Command CheckInCommand
-        {
-            get { return _checkInCommand ?? (_checkInCommand = new Command(async () => await ExecuteCheckInCommandAsync())); }
-        }
-        private async Task ExecuteCheckInCommandAsync()
-        {
-            Debug.WriteLine("ExecuteFetchFeatureShiftCommandAsync");
-            if (IsBusy)
-            {
-                return;
-            }
+		public string FeaturedShiftHours
+		{
+			get
+			{
 
-            IsBusy = true;
+				return FeaturedShift.Label;
+			}
+		}
 
-            DialogService.ShowLoading(string.Empty);
-            var response = await _featuredShiftEndpoint.CheckInAsync<FutureShift>(FeaturedShift.Id);
-            DialogService.HideLoading();
+		private FutureShiftsEndpoint _futureShiftsEndpoint;
+		private FeaturedShiftEndpoint _featuredShiftEndpoint;
 
-            if (response.IsSuccess)
-            {
-                FeaturedShift = response.Result;
-            }
+		public ShiftsViewModel()
+		{
+			_futureShiftsEndpoint = new FutureShiftsEndpoint();
+			_featuredShiftEndpoint = new FeaturedShiftEndpoint();
+			_refreshCommand = new Command(RefreshList);
+		}
 
-            IsBusy = false;
-        }
+		async void RefreshList()
+		{
+			IsRefreshing = true;
+			IsBusy = true;
+			FutureShifts = await ExecuteFetchShiftsLiteAsync();
+			FeaturedShift = await ExecuteFetchFeaturedShiftLiteAsync();
+			IsBusy = false;
+			IsRefreshing = false;
+		}
 
+		Command _fetchAllDataCommand;
+		public Command FetchAllDataCommand
+		{
+			get { return _fetchAllDataCommand ?? (_fetchAllDataCommand = new Command(async () => await ExecuteFetchAllDataCommandAsync())); }
+		}
 
+		private async Task ExecuteFetchAllDataCommandAsync()
+		{
+			if (IsBusy)
+			{
+				return;
+			}
 
-        Command _checkOutCommand;
-        public Command CheckOutCommand
-        {
-            get { return _checkOutCommand ?? (_checkOutCommand = new Command(async () => await ExecuteCheckOutCommandAsync())); }
-        }
-        private async Task ExecuteCheckOutCommandAsync()
-        {
-            Debug.WriteLine("ExecuteFetchFeatureShiftCommandAsync");
-            if (IsBusy)
-            {
-                return;
-            }
+			DialogService.ShowLoading(string.Empty);
+			await Task.WhenAll(MakeFutureShiftsEndpointIndexCall(), MakeFeaturedShiftEndpointFeaturedCall());
+			DialogService.HideLoading();
+			DataLoaded = true;
+			IsBusy = false;
+		}
 
-            IsBusy = true;
+		async Task<List<FutureShift>> ExecuteFetchShiftsLiteAsync()
+		{
+			var response = await _futureShiftsEndpoint.IndexAsync<FutureShift>();
 
-            DialogService.ShowLoading(string.Empty);
-            var response = await _featuredShiftEndpoint.CheckOutAsync<FutureShift>(FeaturedShift.Id);
-            DialogService.HideLoading();
+			return response.Result.ToList();
+		}
 
-            if (response.IsSuccess)
-            {
-                FeaturedShift = response.Result;
-            }
+		async Task<FutureShift> ExecuteFetchFeaturedShiftLiteAsync()
+		{
+			var response = await _featuredShiftEndpoint.FeaturedAsync<FutureShift>();
 
-            IsBusy = false;
-        }
+			return response.Result;
+		}
 
+		Command _checkInCommand;
+		public Command CheckInCommand
+		{
+			get { return _checkInCommand ?? (_checkInCommand = new Command(async () => await ExecuteCheckInCommandAsync())); }
+		}
 
+		private async Task ExecuteCheckInCommandAsync()
+		{
+			Debug.WriteLine("ExecuteFetchFeatureShiftCommandAsync");
+			if (IsBusy)
+			{
+				return;
+			}
 
+			IsBusy = true;
 
+			DialogService.ShowLoading(string.Empty);
+			var response = await _featuredShiftEndpoint.CheckInAsync<FutureShift>(FeaturedShift.Id);
+			DialogService.HideLoading();
 
+			if (response.IsSuccess)
+			{
+				FeaturedShift = response.Result;
+			}
 
+			IsBusy = false;
+		}
 
-        Command _fetchFeaturedShiftCommand;
-        public Command FetchFeaturedShiftCommand
-        {
-            get { return _fetchFeaturedShiftCommand ?? (_fetchFeaturedShiftCommand = new Command(async () => await ExecuteFetchFeaturedShiftCommandAsync())); }
-        }
+		Command _checkOutCommand;
+		public Command CheckOutCommand
+		{
+			get { return _checkOutCommand ?? (_checkOutCommand = new Command(async () => await ExecuteCheckOutCommandAsync())); }
+		}
+		private async Task ExecuteCheckOutCommandAsync()
+		{
+			Debug.WriteLine("ExecuteFetchFeatureShiftCommandAsync");
+			if (IsBusy)
+			{
+				return;
+			}
 
-        private async Task ExecuteFetchFeaturedShiftCommandAsync()
-        {
-            Debug.WriteLine("ExecuteFetchFeatureShiftCommandAsync");
-            if (IsBusy)
-            {
-                return;
-            }
+			IsBusy = true;
 
-            IsBusy = true;
+			DialogService.ShowLoading(string.Empty);
+			var response = await _featuredShiftEndpoint.CheckOutAsync<FutureShift>(FeaturedShift.Id);
+			DialogService.HideLoading();
 
-            DialogService.ShowLoading(string.Empty);
-            var response = await _featuredShiftEndpoint.FeaturedAsync<FutureShift>();
-            DialogService.HideLoading();
+			if (response.IsSuccess)
+			{
+				FeaturedShift = response.Result;
+			}
 
-            if (response.IsSuccess)
-            {
-                FeaturedShift = response.Result;
-            }
+			IsBusy = false;
+		}
 
-            IsBusy = false;
-        }
-    }
+		Command _fetchFeaturedShiftCommand;
+		public Command FetchFeaturedShiftCommand
+		{
+			get { return _fetchFeaturedShiftCommand ?? (_fetchFeaturedShiftCommand = new Command(async () => await ExecuteFetchFeaturedShiftCommandAsync())); }
+		}
+
+		private async Task ExecuteFetchFeaturedShiftCommandAsync()
+		{
+			Debug.WriteLine("ExecuteFetchFeatureShiftCommandAsync");
+			if (IsBusy)
+			{
+				return;
+			}
+
+			IsBusy = true;
+
+			DialogService.ShowLoading(string.Empty);
+			var response = await _featuredShiftEndpoint.FeaturedAsync<FutureShift>();
+			DialogService.HideLoading();
+
+			if (response.IsSuccess)
+			{
+				FeaturedShift = response.Result;
+			}
+
+			IsBusy = false;
+		}
+
+		private async Task MakeFutureShiftsEndpointIndexCall()
+		{
+			var response = await _futureShiftsEndpoint.IndexAsync<FutureShift>();
+			if (response.IsSuccess)
+			{
+				FutureShifts = response.Result.ToList();
+			}
+		}
+
+		private async Task MakeFeaturedShiftEndpointFeaturedCall()
+		{
+			var response = await _featuredShiftEndpoint.FeaturedAsync<FutureShift>();
+			if (response.IsSuccess)
+			{
+				FeaturedShift = response.Result;
+			}
+		}
+	}
 }
