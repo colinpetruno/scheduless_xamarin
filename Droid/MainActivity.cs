b468b393f0896b998c1ba7cfd5df2e7c119b0ad9
Caliber <simon@lazilyinspired.com>
2017-06-19 19:48:20 -0400

    update android app name

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -14,56 +14,56 @@ using Scheduleless.Droid.Fonts;
 
 namespace Scheduleless.Droid
 {
-    [Activity(Label = "Scheduleless.Droid",
-              Icon = "@drawable/icon",
-              Theme = "@style/MyTheme",
-              MainLauncher = true,
-              ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation,
-              ScreenOrientation = ScreenOrientation.Portrait)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
-    {
-        static MainActivity _instance = null;
-        public static MainActivity CurrentActivity
-        {
-            get
-            {
-                return _instance;
-            }
-        }
+	[Activity(Label = "Scheduleless",
+			  Icon = "@drawable/icon",
+			  Theme = "@style/MyTheme",
+			  MainLauncher = true,
+			  ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation,
+			  ScreenOrientation = ScreenOrientation.Portrait)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+	{
+		static MainActivity _instance = null;
+		public static MainActivity CurrentActivity
+		{
+			get
+			{
+				return _instance;
+			}
+		}
 
-        private const string HockeyAppId = "938821bc74244436a8380ee15befc913";
+		private const string HockeyAppId = "938821bc74244436a8380ee15befc913";
 
-        protected override void OnCreate(Bundle bundle)
-        {
-            _instance = this;
+		protected override void OnCreate(Bundle bundle)
+		{
+			_instance = this;
 
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
+			TabLayoutResource = Resource.Layout.Tabbar;
+			ToolbarResource = Resource.Layout.Toolbar;
 
-            UserDialogs.Init(this);
+			UserDialogs.Init(this);
 
-            base.OnCreate(bundle);
+			base.OnCreate(bundle);
 
-            // Load up custom fonts
-            Plugin.Iconize.Iconize
-                  //.With(new IconicModule())
-                  .With(new FontAwesomeModule()
-            );
+			// Load up custom fonts
+			Plugin.Iconize.Iconize
+				  //.With(new IconicModule())
+				  .With(new FontAwesomeModule()
+			);
 
-            // only record crashes if it's on a device
-            if (!Utility.IsEmulator())
-            {
-                CrashManager.Register(this, HockeyAppId, new AppCrashManagerListener());
-            }
+			// only record crashes if it's on a device
+			if (!Utility.IsEmulator())
+			{
+				CrashManager.Register(this, HockeyAppId, new AppCrashManagerListener());
+			}
 
-            global::Xamarin.Forms.Forms.Init(this, bundle);
+			global::Xamarin.Forms.Forms.Init(this, bundle);
 
-            FormsPlugin.Iconize.Droid.IconControls.Init(Resource.Id.toolbar);
+			FormsPlugin.Iconize.Droid.IconControls.Init(Resource.Id.toolbar);
 
-            LoadApplication(new App());
+			LoadApplication(new App());
 
-            // IMPORTANT: Initialize XFGloss AFTER calling LoadApplication on the Android platform
-            XFGloss.Droid.Library.Init(this, bundle);
-        }
-    }
+			// IMPORTANT: Initialize XFGloss AFTER calling LoadApplication on the Android platform
+			XFGloss.Droid.Library.Init(this, bundle);
+		}
+	}
 }
