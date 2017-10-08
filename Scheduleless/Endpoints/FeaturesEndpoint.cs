dd3a28c69fd3287f4418e5a6b172c257b6b8967c
Colin Petruno <colinpetruno@gmail.com>
2017-10-07 22:53:23 -0400

    Feature Toggle Start
    I need to make a service for feature toggles that the app can check to
    enable and disable features.

---
+++ Scheduleless/Endpoints/FeaturesEndpoint.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Scheduleless.Models;
+
+namespace Scheduleless.Endpoints
+{
+  // TODO: RENAME to ShiftsEndpoint
+  public class FeaturesEndpoint
+  {
+    private const string _baseRelativeUrl = "/mobile_api/features";
+
+    public async Task<ApiResponse<Features>> FeaturesAsync<Features>(RequestCachePolicy cachePolicy = RequestCachePolicy.RefreshIfNeeded)
+    {
+      using (var client = new AuthenticatedApiRequest())
+      {
+        Debug.WriteLine("In Features Async Endpoint");
+        return await client.GetAsync<Features>(_baseRelativeUrl, responseMapperKey: "features", cachePolicy: cachePolicy);
+      }
+    }
+  }
+}
---
+++ Scheduleless/Models/Features.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Newtonsoft.Json;
+using Scheduleless.Models.Converters;
+
+namespace Scheduleless.Models
+{
+  public class Features
+  {
+    [JsonProperty("time_clock")]
+    public Boolean TimeClock { get; set; }
+
+    [JsonProperty("trading")]
+    public Boolean Trading { get; set; }
+
+    [JsonProperty("wages")]
+    public Boolean Wages { get; set; }
+  }
+}
